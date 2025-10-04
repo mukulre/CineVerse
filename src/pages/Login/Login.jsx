aac8a6c408f7c908a90b8f4074dd1c056b554563
Mukul Raj P <rajmukul242@gmail.com>
2025-10-04 19:47:13 +0530

    chore(updated Login.css and next is API)

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
+import { useState } from 'react'
 
 const Login = () => {
+
+  const [signState, setSignState] = useState("Sign In")
   return (
     <div className='login'>
       <img src={logo}  className='login-logo' alt="" />
       <div className="login-form">
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type="text" placeholder='Your Name' />
+          {signState === "Sign Up"?<input type="text" placeholder='First Name' />:<></>}
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password' />
-          <button className='signup-btn'>Sign Up</button>
-           <div className="options">
-            <label>
-              <input type="checkbox" className="remember-checkbox" />
-              Remember Me
-            </label>
-            <a href="#">Need Help?</a>
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
             </div>
-        </form>
-        <div className="bottom-text">
-          <p>New to Netflix? <a href='#'>Sign Up</a></p>
-          <p>Already have an account? <a href='#'>Sign In</a></p>
+            <p>Need Help?</p>
+          </div>
+          <div className="form-switch">
+          {signState === "Sign In"?
+          <p>New to Netflix? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up</span></p>
+          :<p>Already have an account? <span onClick={()=>{setSignState("Sign In")}}>Sign In</span></p>
+          }
         </div>
+        </form>
       </div>
     </div>
   )
